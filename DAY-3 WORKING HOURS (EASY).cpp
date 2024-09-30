8fb69c610f86733f2426335c36f2ac2a12afe36b
Knox1008 <narsinghgupta.3301@gmail.com>
2024-09-30 20:15:20 +0530

    Add files via upload

---
+++ DAY-3 WORKING HOURS (EASY).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{   int x,y;
+	    cin>>x>>y;
+	    int total=4*x+y;
+	    cout<<total<<endl;
+	}
+  return 0;
+}

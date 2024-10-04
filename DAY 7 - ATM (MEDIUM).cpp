9c9e77f658f569b7b0f8f5139e134030a535ed58
Knox1008 <narsinghgupta.3301@gmail.com>
2024-10-04 21:34:14 +0530

    Add files via upload

---
+++ DAY 7 - ATM (MEDIUM).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+        vector<int> A(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> A[i];
+        }
+        string result = "";
+        for (int i = 0; i < N; ++i) {
+            if (K >= A[i]) {
+                result += '1';
+                K -= A[i];
+            } else {
+                result += '0';
+            }
+        }
+        cout << result << endl;
+    }
+}

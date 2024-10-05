6110e878d65fe27ace278cd8dbdbcfd63464b0a4
Knox1008 <narsinghgupta.3301@gmail.com>
2024-10-05 21:01:00 +0530

    Add files via upload

---
+++ DAY 8 - MONOPOLY (EASY).cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+	    int p,q,r,s;
+	    cin>>p>>q>>r>>s;
+	    if(p>q+r+s||q>p+r+s||r>p+q+s||s>p+q+r)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}

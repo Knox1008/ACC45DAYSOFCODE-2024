cd25c753219a47e2fbea7016ac2aa519448ccde1
Knox1008 <narsinghgupta.3301@gmail.com>
2024-10-10 21:10:16 +0530

    Add files via upload

---
+++ DAY 13 - EXPERT OR NOT (EASY).cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int T;
+     cin>>T;
+     
+     while(T--)
+     {
+         float X,Y;
+         cin>>X>>Y;
+         if(Y/X>=0.5)
+         {
+             cout<<"YES"<<endl;
+         }
+         else
+         {
+             cout<<"NO"<<endl;
+         }
+         
+     }
+}

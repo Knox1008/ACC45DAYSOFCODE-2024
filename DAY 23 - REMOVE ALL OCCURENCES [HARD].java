3ceb6fddcfbe489b07cfae6b56bfdfdcf8beb735
Knox1008 <narsinghgupta.3301@gmail.com>
2024-10-25 21:38:59 +0530

    Add files via upload

---
+++ DAY 23 - REMOVE ALL OCCURENCES [HARD].java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        int L=part.length();
+
+        while(true)
+        {
+            int index=s.indexOf(part);
+            System.out.println(index);
+            if(index>-1)
+            {
+                s=s.substring(0,index)+s.substring(index+L);
+            }
+            else
+            {
+                break;
+            }
+        }
+        return s;
+    }
+}
